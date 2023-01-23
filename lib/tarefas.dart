caa7e0713c19604ae11b87e92653378162388e7a
Alexander Brandao <alex12a7x@gmail.com>
2023-01-23 18:50:26 -0300

    adicionando dificuldades e estrelas

--- lib/tarefas.dart
+++ lib/tarefas.dart
@@ -27,12 +27,19 @@ class _TarefasState extends State<Tarefas> {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
   const Task(this.nome,{Key? key}) : super(key: key);
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Stack(
@@ -41,30 +48,92 @@ class Task extends StatelessWidget {
             color: Colors.amber,
             height: 140,
           ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.black26,
-                  width: 72,
-                  height: 100,
+          Column(
+            children: [
+              Container(
+                color: Colors.black,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Image.asset("images/lion.png"),
+                      color: Colors.grey,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                              child:
+                              Text(
+                                widget.nome,
+                                style:
+                                const TextStyle(
+                                    fontSize: 24,
+                                    overflow: TextOverflow.ellipsis ),)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+
+                            children: [
+                              Icon(Icons.star, size: 15,color: Colors.amber[100],),
+                              Icon(Icons.star, size: 15,color: Colors.amber[100],),
+                              Icon(Icons.star, size: 15,color: Colors.amber[100],),
+                              Icon(Icons.star, size: 15,color: Colors.amber[100],),
+                              Icon(Icons.star, size: 15,color: Colors.amber[100],),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 52,
+                      height: 52,
+                      child: ElevatedButton(
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber),),
+                          onPressed: (){
+                            setState(() {
+                              nivel++;
+                            });
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.arrow_drop_up),
+                              Text("UP"),
+                            ],
+                          )),
+                    )
+                  ],
                 ),
-                Container(
-                  width: 200,
-                    child:
-                    Text(
-                      nome,
-                      style:
-                      const TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis ),)),
-                ElevatedButton(onPressed: (){}, child: const Icon(Icons.arrow_drop_up))
-              ],
-            ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: nivel/10,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Nivel $nivel", style: TextStyle(fontSize: 20),),
+                  ),
+                ],
+              ),
+            ],
           )
         ],
       ),
